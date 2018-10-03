14ac4f53e502c3a2edc53fba9360d1acb78b6dcd
akhilreddyjirra <akhilreddyjirra@users.noreply.github.com>
2018-10-03 10:56:52 +0530

    Update TestBankAccount.java

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -6,14 +6,9 @@ import org.junit.Test;
 
 public class TestBankAccount {
     @Test
-public void testFunds() {
-        System.out.println("Account intial balance: 10Rs");
+    public void testFunds() {
        BankAccount account = new BankAccount(10);
-       System.out.println("Debitted amount:" + 5.0);
        double amount = account.debit(5);
-       System.out.println("Balance : " +amount);
-       System.out.println("Depositing : " + 100.0);
-       double balance = account.deposit(100);
-       System.out.println("Balance: " + balance);
+       Assert.assertEquals(5.0, amount);
     }
 }
